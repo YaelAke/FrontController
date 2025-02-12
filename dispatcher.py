0ca1940e8967dc31e6b27c630b53863e00618468
YaelAke <al068350@uacam.mx>
2025-02-12 00:22:02 -0600

    Segundo commit

--- dispatcher.py
+++ dispatcher.py
@@ -1,3 +1,4 @@
+# Mapea las opciones a las vistas correspondientes.
 def dispatcher(opcion):
     """ Mapea las opciones a las vistas correspondientes """
     views = {
@@ -6,6 +7,7 @@ def dispatcher(opcion):
         "profile": "profile.html",
         "inicio": "index.html"
     }
+    
     if opcion is None:  
         return "index.html"
     return views.get(opcion, "404.html")  # Si la opción no es válida, devuelve al error 404.html
--- front_controller.py
+++ front_controller.py
@@ -3,14 +3,18 @@ from dispatcher import dispatcher
 
 app = Flask(__name__)
 
+#Controlador que maneja las vistas según la opción
 @app.route('/')
 def front_controller():
-    """ Controlador frontal que maneja las vistas según la opción en la URL. """
-    opcion = request.args.get("opcion")  
+    opcion = request.args.get("opcion")
     template = dispatcher(opcion)  # Usa el dispatcher para elegir la vista
     return render_template("base.html", content_template=template)
 
+
+#Manejo de errores
 @app.errorhandler(404)
 def page_not_found(error):
-    """ Manejo global de errores 404 """
     return render_template("base.html", content_template="404.html"), 404
+
+if __name__ == '__main__':
+    app.run(debug=True)
